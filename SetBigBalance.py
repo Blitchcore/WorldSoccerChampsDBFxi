b1943363a006a087302c991e98866b2c9482a293
Vladimir <131596433+Blitchcore@users.noreply.github.com>
2025-08-01 12:24:36 +0600

    Added "SET BIG BALANCE"

---
+++ SetBigBalance.py
@@ -0,0 +1,27 @@
+import sqlite3
+
+#Created By BLITCHCORE 
+#github.com/blitchcore/
+
+def abc(db_path):
+    try:
+        conn = sqlite3.connect(db_path)
+        cursor = conn.cursor()
+        
+        cursor.execute("UPDATE Club SET TransferBudget = 999999999, Balance = 999999999 WHERE id != 0")
+        conn.commit()
+        
+        changes = cursor.rowcount
+        print(f"Успешно обновлено {changes} записей.")
+        
+    except sqlite3.Error as e:
+        print(f"er: {e}")
+    finally:
+        if conn:
+            conn.close()
+print("github.com/blitchcore") 
+print("/nDefault DB Name = datapack.db")
+path = input("void db name: ")  # dbname
+if not path:
+    path = "datapack.db"
+abc(path)
